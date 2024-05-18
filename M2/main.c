e92bd358a4e8613c4cbe4530f8a36dad33f1cb20
Ahmed Hawater <hawatercoding@gmail.com>
2024-05-18 19:09:54 +0300

    Add MLFQ scheduler logic (need debugging)

--- M2/main.c
+++ M2/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define PCB_SIZE 6
 #define NUM_VARIABLES_PER_PROCESS 3
+#define NUM_QUEUES 4
+
+// globally defined to be automatically initialized
 
 char **read_program(const char *filename, int *num_lines)
 {
@@ -65,7 +69,7 @@ char **read_program(const char *filename, int *num_lines)
     return lines;
 }
 
-char **spli1t_instruction(const char *str, int *num_words)
+char **split_instruction(const char *str, int *num_words)
 {
     char **words;
     int max_words = 16;
@@ -154,40 +158,159 @@ typedef struct
 
 typedef struct
 {
-    int pid;
     int arrival_time;
     int number_of_instructions;
+    Pair *pid;
     Pair *pc;
     Pair *state;
     Pair *priority;
     Pair *start_of_variables_section;
 } ProcessNeededInformation;
 
-void init_PCB(Memory *memory, int process_id)
+typedef struct
 {
+    ProcessNeededInformation *processes[100]; // Array to hold processes in the queue
+    int count;                                // Number of processes in the queue
+} Queue;
+
+typedef struct
+{
+    Queue level_queues[NUM_QUEUES];
+    Queue ready_queue;
+    ProcessNeededInformation *running_process;
+    int quantum;
+} MLFQ;
+
+Memory memory;
+MLFQ scheduler_queue;
+
+ProcessNeededInformation *dequeue(Queue *queue)
+{
+    if (queue->count == 0)
+        return NULL;
+    ProcessNeededInformation *process = queue->processes[0];
+    for (int i = 1; i < queue->count; i++)
+    {
+        queue->processes[i - 1] = queue->processes[i];
+    }
+    queue->count--;
+    return process;
+}
+
+void enqueue(Queue *queue, ProcessNeededInformation *process)
+{
+    queue->processes[queue->count++] = process;
+}
+
+void push_to_ready_queue(ProcessNeededInformation *process)
+{
+    enqueue(&(scheduler_queue.ready_queue), process);
+}
+
+void add_ready_processes()
+{
+    for (int i = 0; i < scheduler_queue.ready_queue.count; i++)
+    {
+        ProcessNeededInformation *process = dequeue(&(scheduler_queue.ready_queue));
+        int priority_level = to_int(process->priority->value);
+        enqueue(&(scheduler_queue.level_queues[priority_level]), process);
+    }
+}
+
+ProcessNeededInformation *get_highest_priority_process(int *quantum)
+{
+    for (int i = 0; i < NUM_QUEUES; i++)
+    {
+        if (scheduler_queue.level_queues[i].count > 0)
+        {
+            *quantum = 1 << i;
+            return dequeue(&(scheduler_queue.level_queues[i]));
+        }
+    }
+    *quantum = 0;
+    return NULL;
+}
+
+ProcessNeededInformation *pull_executable_process()
+{
+    add_ready_processes();
+    if (scheduler_queue.quantum == 0)
+    {
+        if (scheduler_queue.running_process != NULL)
+        {
+            scheduler_queue.running_process->priority->value = to_string(to_int(scheduler_queue.running_process->priority->value) + 1);
+        }
+
+        scheduler_queue.running_process = get_highest_priority_process(&(scheduler_queue.quantum));
+    }
+    else
+    {
+        if (strcmp(scheduler_queue.running_process->state->value, "blocked") == 0)
+        {
+            scheduler_queue.running_process = get_highest_priority_process(&(scheduler_queue.quantum));
+        }
+        else
+        {
+            scheduler_queue.quantum--;
+        }
+    }
+}
+
+void print_PNI(ProcessNeededInformation *process)
+{
+    printf("Process ID : %s\n", process->pid->value);
+    printf("Priority: %s\n", process->priority->value);
+}
+
+void print_MLFQ()
+{
+
+    for (int i = 0; i < NUM_QUEUES; i++)
+    {
+        printf("----------------------------------- Queue %d ---------------------------------\n", i);
+        if (scheduler_queue.level_queues[i].count > 0)
+        {
+            int const count = scheduler_queue.level_queues[i].count;
+            for (int j = 0; j < count; j++)
+            {
+                print_PNI(scheduler_queue.level_queues[i].processes[j]);
+            }
+        }
+        printf("--------------------------------------------------------------------------------\n");
+    }
+}
+
+void init_PCB(int process_id, ProcessNeededInformation *process)
+{
+
+    process->pid = &memory.cells[(memory.number_of_populated_cells)];
+    process->priority = &memory.cells[(memory.number_of_populated_cells) + 2];
+
     Pair pcb[PCB_SIZE] =
         {
             {"ID", to_string(process_id)},
             {"State", "Ready"},
             {"Priority", "0"},
-            {"PC", to_string(memory->number_of_populated_cells + 9)},
-            {"Lower Bound", to_string(memory->number_of_populated_cells)},
-            {"Upper Bound", to_string(memory->number_of_populated_cells)}};
+            {"PC", to_string(memory.number_of_populated_cells + 9)},
+            {"Lower Bound", to_string(memory.number_of_populated_cells)},
+            {"Upper Bound", to_string(memory.number_of_populated_cells)}};
 
     for (int i = 0; i < PCB_SIZE; i++)
-        memory->cells[(memory->number_of_populated_cells)++] = pcb[i];
+        memory.cells[(memory.number_of_populated_cells)++] = pcb[i];
 }
 
-void init_variables_section(Memory *memory)
+void init_variables_section()
 {
     for (int i = 0; i < NUM_VARIABLES_PER_PROCESS; i++)
-        (memory->number_of_populated_cells)++;
+        (memory.number_of_populated_cells)++;
 }
 
-void init_process_instructoins(Memory *memory, char *program_file_path)
+void init_process_instructoins(char *program_file_path)
 {
     int num_lines;
     char **lines = read_program(program_file_path, &num_lines);
+    const int upper_bound_index = (memory.number_of_populated_cells) - 4;
+    memory.cells[upper_bound_index].value = to_string((memory.number_of_populated_cells) + num_lines - 1);
     if (lines == NULL)
     {
         printf("Error reading program file\n");
@@ -201,36 +324,35 @@ void init_process_instructoins(Memory *memory, char *program_file_path)
         sprintf(instructoin_number, "Inst_%d", i);
         sprintf(instruction, "%s", lines[i]);
 
-        memory->cells[(memory->number_of_populated_cells)++] = (Pair){instructoin_number, instruction};
+        memory.cells[(memory.number_of_populated_cells)++] = (Pair){instructoin_number, instruction};
     }
 
     clear_buffer(lines, num_lines);
 }
 
-void add_process(Memory *memory, char *program_file_path, int process_id, int clk)
+void add_process(char *program_file_path, int process_id, int clk)
 {
-    init_PCB(memory, process_id);
+    ProcessNeededInformation *p = malloc(sizeof(ProcessNeededInformation));
+    init_PCB(process_id, p);
     init_variables_section(memory);
-    init_process_instructoins(memory, program_file_path);
+    init_process_instructoins(program_file_path);
+    push_to_ready_queue(p);
 }
 
-void print_mem(Memory *memory)
+void print_mem()
 {
     for (int i = 0; i < 60; i++)
     {
-        printf("%s: %s\n", (memory->cells[i]).name, (memory->cells[i]).value);
+        printf("%s: %s\n", (memory.cells[i]).name, (memory.cells[i]).value);
     }
 }
-
-Memory memory; // globally defined to be automatically initialized
 
 void main()
 {
 
-    int processs_id = 0;
-
-    add_process(&memory, "Program_1.txt", processs_id++, 0);
-    add_process(&memory, "Program_2.txt", processs_id++, 0);
-    add_process(&memory, "Program_3.txt", processs_id++, 0);
-    print_mem(&memory);
+    add_process("Program_1.txt", 0, 0);
+    add_process("Program_2.txt", 1, 0);
+    print_mem();
+    pull_executable_process();
+    print_MLFQ();
 }
